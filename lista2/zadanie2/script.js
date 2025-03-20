d5b1a5a5a74c4db73b8c70dbe504222e94c51643
Daniel <danielryszkowski@gmail.com>
2025-03-20 10:58:07 +0100

    lista2 -> small corrections

--- lista2/zadanie2/script.js
+++ lista2/zadanie2/script.js
@@ -15,8 +15,14 @@ document.addEventListener("DOMContentLoaded", function () {
         const height = parseFloat(document.getElementById("height").value);
         const depth = parseFloat(document.getElementById("depth").value);
 
+        //Walidaja poprawnośći nazwy (czy nie jest puste i czy jest nie dłuższe niż 20 znaków)
+        if(!name || name.length > 20){
+            alert("Name is empty or longer than 20 characters.");
+            return;
+        }
+
         // Walidacja poprawnych wartości (czy nie są puste i mieszczą się w zakresie 1-1000 cm)
-        if (!name || isNaN(width) || isNaN(height) || isNaN(depth) || width <= 0 || height <= 0 || depth <= 0) {
+        if (isNaN(width) || isNaN(height) || isNaN(depth) || width <= 0 || height <= 0 || depth <= 0 || width > 1000 || depth > 1000 || height > 1000) {
             alert("Please enter valid dimensions (1-1000 cm)!");
             return;
         }
@@ -46,11 +52,19 @@ document.addEventListener("DOMContentLoaded", function () {
     searchInput.addEventListener("input", function () {
         const filter = searchInput.value.toLowerCase(); // Pobranie wartości wpisanej w wyszukiwarkę
         const rows = packageList.getElementsByTagName("tr"); // Pobranie wszystkich wierszy tabeli
+        let filteredTotalVolume = 0; // Zmienna do przechowywania sumarycznej objętości widocznych paczek
 
         // Iteracja po wierszach i filtrowanie paczek
         for (let row of rows) {
             let name = row.cells[0].textContent.toLowerCase(); // Pobranie nazwy paczki z tabeli
-            row.style.display = name.includes(filter) ? "" : "none"; // Ukrywanie/wyświetlanie wierszy
+            if (name.includes(filter)) {
+                row.style.display = ""; // Wyświetlanie wierszy
+                filteredTotalVolume += parseFloat(row.cells[4].textContent); // Dodawanie objętości widocznych paczek
+            } else {
+                row.style.display = "none"; // Ukrywanie wierszy
+            }
         }
+
+        totalVolumeEl.textContent = filteredTotalVolume.toFixed(2); // Aktualizacja sumarycznej objętości w stopce tabeli
     });
 });
